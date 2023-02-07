9deffb3c833e7f21c7d3c64837b31003c29a6ddd
RomahaVO <romahabodnaruk@icloud.com>
2023-02-07 21:16:29 +0200

    markup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
+import fetchCountries from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const search = document.getElementById('search-box');
+
+function createMarkup () {
+    const markup = 
+    `<li><img src='${country.flags.svg}' alt='flag' width='30' class='country-flag'/>
+    <h2>${country.name.common}</h2>
+    <p>Capital: ${country.capital}</p>
+    <p>Population: ${country.population}</p>
+    <p>Languages: ${allLanguage}</p></li>`;
+    console.log(markup);
+};
+
+function createMarkupMin() {
+    
+};
+
