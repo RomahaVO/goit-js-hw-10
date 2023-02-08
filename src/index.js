b20b9c0c4ed22d9a281ec11c2e059597be59680e
RomahaVO <romahabodnaruk@icloud.com>
2023-02-08 21:03:04 +0200

    pushFetch

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import fetchCountries from './js/fetchCountries';
+import countries from './js/fetchCountries';
+import Notiflix from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
 
-const search = document.getElementById('search-box');
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-function createMarkup () {
-    const markup = 
-    `<li><img src='${country.flags.svg}' alt='flag' width='30' class='country-flag'/>
-    <h2>${country.name.common}</h2>
-    <p>Capital: ${country.capital}</p>
-    <p>Population: ${country.population}</p>
-    <p>Languages: ${allLanguage}</p></li>`;
-    console.log(markup);
+
+input.addEventListener('input', debounce(onInputChange,DEBOUNCE_DELAY));
+
+function onInputChange(e){
+e.preventDefault();
+
+const searchCountry = e.target.value.trim();
+
+    if (searchCountry === "") {
+        countryInfo.innerHTML = "";
+        countryList.innerHTML = "";
+        return;
+    };
+
+countries.fetchCountries(searchCountry).then(country => {
+
+    if (country.length >= 11){
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        countryList.innerHTML = "";
+        return;
+    }
+
+    if (country.length >= 2){
+        createList(country);
+        countryInfo.innerHTML = "";
+        return;
+    }
+
+    if (country.length === 1){
+        createCard(country[0]);
+        countryList.innerHTML = "";
+        return;
+    }
+
+    if (country.status === 404) {
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+        countryList.innerHTML = "";
+        countryInfoCard.innerHTML = "";
+    }     
+
+});
 };
 
-function createMarkupMin() {
+function createCard({languages, flags, name, capital, population}){
+    const markup = `<h2><img src='${flags.svg}' alt='flag' width='30' class='country-flag'/>${name.common}</h2>
+        <p>Capital: ${capital}</p>
+        <p>Population: ${population}</p>
+        <p>Languages: ${Object.values(languages)}</p>
+        `;
     
+    countryInfo.innerHTML = markup;
 };
 
+function createList(country){
+    const list = country.map(name =>
+    `<li class='list-elem'>
+    <p><img src='${name.flags.svg}' alt='flag' width='30' class='country-flag'/>${name.name.common}<p>
+    </li>`).join('');
+    countryList.innerHTML = list;
+};
